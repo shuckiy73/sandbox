45a1d16a63f4b6703d9f345beb97e56ecc3c5fc0
shuckiy73 <121363089+shuckiy73@users.noreply.github.com>
2023-12-17 17:39:43 +0300

    Функция 1 - привет, мир

---
+++ codewars/8kyu/task5.js
@@ -0,0 +1,8 @@
+// ОПИСАНИЕ:
+// Создаем простую функцию с именем greet, которая возвращает самое известное "hello world!".
+
+function greet() {
+    return "hello world!"; 
+  }
+  
+  console.log(greet());
